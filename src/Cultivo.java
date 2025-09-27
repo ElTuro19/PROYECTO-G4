45e080968cec521a11ec0c2aec52ea35bd67c06d
Arturo Godoy <arturo.godoy2501@alumnos.ubiobio.cl>
2025-09-26 21:25:41 -0300

    Muestra de avance en la clase Cultivo

--- src/Cultivo.java
+++ src/Cultivo.java
@@ -1,2 +1,33 @@
 public class Cultivo {
+    private int id;
+    private String especie;
+    private String variedad;
+    private float rendimiento;
+
+    public Cultivo(int id, String especie, String variedad, float rendimiento) {
+        this.id = id;
+        this.especie = especie;
+        this.variedad = variedad;
+        this.rendimiento = rendimiento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getEspecie() {
+        return especie;
+    }
+
+    public String getVariedad() {
+        return variedad;
+    }
+
+    public float getRendimiento() {
+        return rendimiento;
+    }
+
+    public void setRendimiento(float rendimiento) {
+        this.rendimiento = rendimiento;
+    }
 }
