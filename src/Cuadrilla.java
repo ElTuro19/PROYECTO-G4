9b96bfa947a4b409f291a7b16d3bfdf81e114c39
aleja <alejandra.quiroga2401@alumnos.ubiobio.cl>
2025-09-30 13:14:47 -0300

    Archivo Cuadrilla

---
+++ src/Cuadrilla.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Cuadrilla {
+
+    private int id;
+    private String nombre;
+    private int maximoCosechadores;
+
+    private Supervisor supervisor;
+    private PlanCosecha planCosecha;
+    private ArrayList<Cosechador>cosechadores;
+
+    public Cuadrilla(int id, String nom, Supervisor sup, PlanCosecha plan){
+
+        this.id = id;
+        this.nombre = nom;
+        this.supervisor = sup;
+        this.planCosecha = plan;
+        this.cosechadores = new ArrayList<>();
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public String setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public Supervisor getSupervisor() {
+        return supervisor;
+    }
+
+    public PlanCosecha getPlanCosecha() {
+        return planCosecha;
+    }
+
+    public boolean addCosechador(Date fIni, Date fFin, double meta, Cosechador cos){
+
+       if (cosechadores.size() < maximoCosechadores){
+           cosechadores.add(cos);
+           return true;
+       }
+       return false;
+    }
+    public Cosechador[] getCosechadores(){
+        return cosechadores.toArray(new Cosechador[0]);
+    }
+    public int getMaximoCosechadores(){
+        return maximoCosechadores;
+    }
+    public void setMaximoCosechadores(int max){
+        this.maximoCosechadores = max;
+    }
+}
