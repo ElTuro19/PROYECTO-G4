6bbf191e3d7875fce324ba8c3354fd6a9817e47f
aleja <alejandra.quiroga2401@alumnos.ubiobio.cl>
2025-10-02 12:30:00 -0300

    Archivo cosechador comentarios

--- src/Cosechador.java
+++ src/Cosechador.java
@@ -3,9 +3,9 @@ import java.util.Date;
 import java.util.List;
 
 public class Cosechador {
-
+    //Atributo
     private Date fechaNacimiento;
-
+    //Guarda las asignaciones de cosechador en las distintas cuadrillas
     private final List<CosechadorAsignado>asignaciones = new ArrayList<>();
 
     public Date getFechaNacimiento(){
@@ -15,12 +15,12 @@ public class Cosechador {
     public void setFechaNacimiento(Date fNac) {
         this.fechaNacimiento = fNac;
     }
-
+    //Agregar un cosechador a la cuadrilla
     public void addCuadrilla(CosechadorAsignado cosAs){
         if (cosAs == null) return;
         asignaciones.add(cosAs);
     }
-
+    //Muestra las cuadrillas a las que está asociado
     public Cuadrilla[] getCuadrillas(){
         return asignaciones.stream().map(CosechadorAsignado::getCuadrilla).toArray(Cuadrilla[]::new);
     }
