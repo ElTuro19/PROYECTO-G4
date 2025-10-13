b46aa8808564f35a76c97a01ce93cb97700d806f
ale <alexis.stockebrand2501@alumnos.ubiobio.cl>
2025-10-13 12:51:50 -0300

    avance GestionHuertosApp

--- src/GestionHuertosApp.java
+++ src/GestionHuertosApp.java
@@ -1,3 +1,4 @@
+import java.util.Date;
 import java.util.Scanner;
 import java.util.ArrayList;
 public class GestionHuertosApp {
@@ -24,44 +25,91 @@ public class GestionHuertosApp {
         System.out.println("9. Listar Planes de Cosecha");
         System.out.println("10. Salir");}
     public void creaPersona() {
+        String strOpt = "";
         boolean success = false;
-        System.out.println("Creando una persona . . .");
-        System.out.print("Rol persona (1=Propietario, 2=Supervisor, 3=Cosechador): ");
         do {
+            System.out.println("Creando una persona . . .");
+            System.out.print("Rol persona (1=Propietario, 2=Supervisor, 3=Cosechador): ");
             int opt = sc.nextInt();
+            System.out.print("Rut : ");
+            Rut rut = sc.next();
+            System.out.print("Nombre: ");
+            String nombre = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.nextLine();
+            System.out.print("Direccion:");
+            String direccion = sc.nextLine();
+            System.out.print("Fecha de Nacimiento (dd/mm/aaaa): ");
         switch (opt) {
             case 1:
-            cp.createPropietario()
+                System.out.print("Direccion Profesional: ");
+                String dirProf = sc.nextLine();
+                success = cp.createPropietario(rut, nombre, email, direccion, dirProf);
+                strOpt = "Propietario";
+                break;
             case 2:
+                System.out.print("Profesion: ");
+                String Prof = sc.nextLine();
+                success = cp.createSupervisor(rut, nombre, email, direccion, Prof);
+                strOpt = "Supervisor";
+                break;
             case 3:
+                System.out.print("Fecha Nacimiento: ");
+                Date dirProf = sc.next();
+                success = cp.createCosechador(rut, nombre, email, direccion, dirProf);
+                strOpt = "Cosechador";
+                break;
             default:
-        }} while (success=false)
-        System.out.print("Rut : ");
-        Rut rut
-        System.out.print("Nombre: ");
-        System.out.print("Email: ");
-        System.out.print("Direccion:");
-        System.out.print("Fecha de Nacimiento (dd/mm/aaaa): ");
-    }
-    public void creaCultivo() {
-        System.out.println("Creando un Cultivo . . .");
-        System.out.print("Identificacion: ");
-        System.out.print("Especie: ");
-        System.out.print("Variedad: ");
-        System.out.print("Rendimiento: ");
+                System.out.println("no existe esa opcion. . ");
+                break;
+        }
+        if (success==false) {System.out.println("Crear "+strOpt+" a fallado");}
+        } while (success!=true);
+        System.out.println(+strOpt+" creado exitosamente"
 
     }
+    public void creaCultivo() {
+        boolean success = false;
+        int id;
+        String especie, variedad;
+        float perf;
+        do {
+        System.out.println("Creando un Cultivo . . .");
+        System.out.print("Identificacion: ");
+        id = sc.nextInt();
+        System.out.print("Especie: ");
+        especie = sc.nextLine();
+        System.out.print("Variedad: ");
+        variedad = sc.nextLine();
+        System.out.print("Rendimiento: ");
+        perf = sc.nextFloat();}
+        success = cp.createCultivo(id, especie, variedad, perf);
+        if (success == false) {System.out.println("No se puede crear un cultivo con un id de uno que ya existe");}
+        while (success!=true);
+        System.out.println("Cultivo creado exitosamente");
+    }
     public void creaHuerto() {
+        boolean success = false;
+        String nom, ubi;
+        float sup;
+        Rut rut;
+        do {
         System.out.println("Creando un Huerto . . .");
         System.out.print("Nombre: ");
+        nom = sc.nextLine();
         System.out.print("Superficie: ");
+        sup = sc.nextFloat();
         System.out.print("Ubicacion: ");
+        ubi = sc.nextLine();
         System.out.print("Rut Propietario: ");
+
         System.out.println("Agregando Cuarteles al Huerto . . .");
         System.out.print("Nro. de Cuarteles: ");
         System.out.print("Id Cuartel: ");
         System.out.print("Superficie Cuartel: ");
-        System.out.print("Id cultivo del cuartel: ");
+        System.out.print("Id cultivo del cuartel: ");}
+        success=cp.createHuerto(nom, sup, ubi, rutPr);
+        while (success!=true)
     }
     public void creaPlanDeCosecha() {
         System.out.println("Creando un plan de cosecha...");
