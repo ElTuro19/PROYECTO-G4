3f2b122325f483c8c8158765b343ca899a6bc412
alejandraquiroga2401-spec <alejandra.quiroga2401@alumnos.ubiobio.cl>
2025-12-18 22:50:44 -0300

    Update CosechadorAsignado.java

--- src/modelo/CosechadorAsignado.java
+++ src/modelo/CosechadorAsignado.java
@@ -1,4 +1,10 @@
 package modelo;
+/// utilidades
+import utilidades.Calidad;
+import utilidades.EstadoFenologico;
+import utilidades.EstadoPlan;
+import utilidades.GestionHuertosException;
+import utilidades.Rut;
 
 import java.time.LocalDate;
 
@@ -7,19 +13,45 @@ public class CosechadorAsignado {
     private LocalDate desde;
     private LocalDate hasta;
     private double metaKilos;
+
     private Cuadrilla cuadrilla;
     private Cosechador cosechador;
 
-    public CosechadorAsignado(LocalDate d, LocalDate h, double meta, Cuadrilla c, Cosechador cos) {
-        desde = d;
-        hasta = h;
-        metaKilos = meta;
-        cuadrilla = c;
-        cosechador = cos;
+    public CosechadorAsignado(LocalDate fIni, LocalDate fFin, double meta, Cuadrilla cuad, Cosechador cos){
+        this.desde = fIni;
+        this.hasta = fFin;
+        this.metaKilos = meta;
+        this.cuadrilla = cuad;
+        this.cosechador = cos;
     }
 
-    public LocalDate getDesde() { return desde; }
-    public LocalDate getHasta() { return hasta; }
-    public Cuadrilla getCuadrilla() { return cuadrilla; }
-    public Cosechador getCosechador() { return cosechador; }
+    public LocalDate getDesde() {
+        return desde;
+    }
+
+    public void setDesde(LocalDate desde) { this.desde = desde;}
+
+    public LocalDate getHasta() {
+        return hasta;
+    }
+
+    public void setHasta(LocalDate hasta) {
+        this.hasta = hasta;
+    }
+
+    public double getMetaKilos() {
+        return metaKilos;
+    }
+
+    public void setMetaKilos(double metaKilos) {
+        this.metaKilos = metaKilos;
+    }
+
+    public Cuadrilla getCuadrilla() {
+        return cuadrilla;
+    }
+
+    public Cosechador getCosechador() {
+        return cosechador;
+    }
 }
